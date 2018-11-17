366685f83e1e1e2c226dfad5f8c4f6f08a95bee3
juritobi <pmijoijab@gmail.com>
2018-11-17 20:18:17 +0100

    actualizando el proyecto completp

---
+++ Assets/hero.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hero : MonoBehaviour {
+	public Slider barH;
+	public Slider barM;
+	public int health;
+	public int mana;
+	public bool clicked;
+	public bool dead;
+
+	// Use this for initialization
+	void Start () {
+		health = 100;
+		mana = 100;
+		clicked = false;
+		dead = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		barH.value=health;
+		barM.value=mana;
+		if(barH.value==0){
+			this.DestroyGameObject();
+		}
+	}
+
+	public void attack(enemy e, int atc){
+		if(atc==1){
+			e.recibeDamage(25);
+		}
+
+		if(atc==2){
+			e.recibeDamage(50);
+			mana-=25;
+		}
+	}
+
+	public void recibeDamage(int n){
+		health-=n;
+		if(health<=0){
+			health = 0;
+		}
+	}
+
+	void OnMouseDown(){
+		clicked = true;
+	}
+
+	void DestroyGameObject(){
+		Destroy(barH);
+		Destroy(barM);
+		Destroy(gameObject);   //metodo que elimina el objeto
+	}
+}
---
+++ Assets/main.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class main : MonoBehaviour {
+	public enemy enemigo;
+	public hero heroe1; public hero heroe2; public hero heroe3;
+	public Button ataque;
+	public Button habilidad;
+
+	public enum BattleStates
+    {
+        ENEMYC,
+        PLAYER1C,
+        PLAYER2C,
+        PLAYER3C,
+        VICTORY
+    }
+
+	private BattleStates currentState;
+
+	// Use this for initialization
+	void Start () {
+		currentState = BattleStates.ENEMYC;
+		ataque.onClick.AddListener(ataqueClick);
+		habilidad.onClick.AddListener(habilidadClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (currentState){
+			case (BattleStates.ENEMYC):
+				Debug.Log("Turno Enemigo");
+				break;
+			
+			case (BattleStates.PLAYER1C):
+				Debug.Log("Turno Player1");
+				//StartCoroutine(waiter());
+				waiter();
+				heroe1.attack(enemigo,2);
+				currentState = BattleStates.PLAYER2C;
+				break;
+			
+			case (BattleStates.PLAYER2C):
+				Debug.Log("Turno Player2");
+				heroe2.attack(enemigo,1);
+				currentState = BattleStates.PLAYER3C;
+				break;
+			
+			case (BattleStates.PLAYER3C):
+				Debug.Log("Turno Player3");
+				heroe3.attack(enemigo,1);
+				currentState = BattleStates.ENEMYC;
+				break;
+		}
+		//ataque.onClick.AddListener(ataqueClick);
+	}
+
+	void ataqueClick(){
+		if(currentState==BattleStates.ENEMYC){
+			if(heroe1.clicked==true){
+				enemigo.attack(heroe1,1);
+				heroe1.clicked=false;
+				heroe2.clicked=false;
+				heroe3.clicked=false;
+				currentState = BattleStates.PLAYER1C;
+			}
+
+			if(heroe2.clicked==true){
+				enemigo.attack(heroe2,1);
+				heroe1.clicked=false;
+				heroe2.clicked=false;
+				heroe3.clicked=false;
+				currentState = BattleStates.PLAYER1C;
+			}
+
+			if(heroe3.clicked==true){
+				enemigo.attack(heroe3,1);
+				heroe1.clicked=false;
+				heroe2.clicked=false;
+				heroe3.clicked=false;
+				currentState = BattleStates.PLAYER1C;
+			}
+		}
+	}
+
+	void habilidadClick(){
+		if(currentState==BattleStates.ENEMYC){
+			if(heroe1.clicked==true){
+				enemigo.attack(heroe1,2);
+				heroe1.clicked=false;
+				heroe2.clicked=false;
+				heroe3.clicked=false;
+				currentState = BattleStates.PLAYER1C;
+			}
+
+			if(heroe2.clicked==true){
+				enemigo.attack(heroe2,2);
+				heroe1.clicked=false;
+				heroe2.clicked=false;
+				heroe3.clicked=false;
+				currentState = BattleStates.PLAYER1C;
+			}
+
+			if(heroe3.clicked==true){
+				enemigo.attack(heroe3,2);
+				heroe1.clicked=false;
+				heroe2.clicked=false;
+				heroe3.clicked=false;
+				currentState = BattleStates.PLAYER1C;
+			}
+		}
+	}
+	IEnumerator waiter(){
+		yield return new WaitForSeconds(15);
+	}
+}
