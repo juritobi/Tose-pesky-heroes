cbb8c7e35d75140e23226884cfd2aec7d10143db
xiviwillburn <ivpr199@gmail.com>
2018-12-07 13:16:30 +0100

    Main actualizado, tenia algunos bugs en los comentarios

--- Assets/Scrips/main.cs
+++ Assets/Scrips/main.cs
@@ -7,45 +7,88 @@ using System.Threading;
 using System;
 using TMPro;
 
-public class main : MonoBehaviour {
-	public personaje player;
-	//public Object[] heroes= FindObjectsOfType(Type hero);
-	public arqueroBase dps; public tankBase tank; public healerBase healer;public espadachinBase espadachin;
-	public npc[] all;
-	public Button ataque;
-	public Button habilidad;
+public class main : MonoBehaviour
+{
+    public personaje player;
+
+    //public Object[] heroes= FindObjectsOfType(Type hero);
+
+    public arqueroBase dps;
+    public tankBase tank;
+    public healerBase healer;
+    public espadachinBase espadachin;
+
+    public npc[] all;
+
+    public Button Accion1;
+    public Button Accion2;
+    public Button Accion3;
+    public Button Accion4;
+    public Button Accion5;
+    public Button Accion6;
+
     public bool paused = false;
 
-	public enum BattleStates
+
+
+
+    public enum BattleStates
     {
         ENEMYC,
         PLAYER1C,
         PLAYER2C,
         PLAYER3C,
         PLAYER4C,
+        DEFEAT,
         VICTORY
     }
 
-	private BattleStates currentState;
+    private BattleStates currentState;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         all = FindObjectsOfType<npc>();
-		currentState = BattleStates.ENEMYC;
-		ataque.onClick.AddListener(ataqueClick);
-		habilidad.onClick.AddListener(habilidadClick);
-	}
 
-	// Update is called once per frame
-	void Update () {
-		switch (currentState){
-			case (BattleStates.ENEMYC):
+        currentState = BattleStates.ENEMYC;
+
+        Accion1.onClick.AddListener(AccionA);
+        Accion2.onClick.AddListener(AccionB);
+        Accion3.onClick.AddListener(AccionC);
+        Accion4.onClick.AddListener(AccionD);
+        Accion5.onClick.AddListener(AccionE);
+        Accion6.onClick.AddListener(AccionF);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (currentState)
+        {
+
+            case (BattleStates.ENEMYC):
+
+                if (AllTargetsDestroyed())
+                {
+                    currentState = BattleStates.VICTORY;
+                }
+
+                if (player.getDead())
+                {
+                    currentState = BattleStates.DEFEAT;
+                }
+
+
                 paused = false;
-				break;
+                break;
 
-			case (BattleStates.PLAYER1C):
+            case (BattleStates.PLAYER1C):
                 if (tank.getDead())
                 {
+                    tank.clicked = false;
                     currentState = BattleStates.PLAYER2C;
                     paused = true;
                 }
@@ -61,8 +104,9 @@ public class main : MonoBehaviour {
             case (BattleStates.PLAYER2C):
                 if (dps.getDead())
                 {
+
                     currentState = BattleStates.PLAYER3C;
-                    paused=false;
+                    paused = false;
                 }
                 else if (paused)
                 {
@@ -71,9 +115,9 @@ public class main : MonoBehaviour {
                     StartCoroutine(waiter());
                     paused = false;
                 }
-				break;
+                break;
 
-			case (BattleStates.PLAYER3C):
+            case (BattleStates.PLAYER3C):
                 if (healer.getDead())
                 {
                     currentState = BattleStates.PLAYER4C;
@@ -86,7 +130,7 @@ public class main : MonoBehaviour {
                     StartCoroutine(waiter());
                     paused = true;
                 }
-				break;
+                break;
             case (BattleStates.PLAYER4C):
                 if (espadachin.getDead())
                 {
@@ -98,13 +142,312 @@ public class main : MonoBehaviour {
                     espadachin.restaCooldowns();
                     espadachin.decision();
                     StartCoroutine(waiter());
-                    
+
                     paused = false;
                 }
                 break;
+
+            case (BattleStates.VICTORY):    //  implementar plss
+
+                //  Mostrar pantalla de victoria
+                //  Recargar los npcs
+
+
+                break;
+
+            case (BattleStates.DEFEAT):     //  implementar plss
+
+                //  Mostrar pantalla derrota
+                //  Ir al menu principal
+
+                break;
+
         }
-		//ataque.onClick.AddListener(ataqueClick);
-	}
+
+        //ataque.onClick.AddListener(ataqueClick);
+    }
+    //  clicked seleccionado
+    void AccionA()
+    {
+
+        if (currentState == BattleStates.ENEMYC)
+        {
+
+            npc Target = null;
+
+            for (int i = 0; i < all.Length; i++)
+            {
+                if (all[i].clicked)
+                {
+                    Target = all[i];
+
+                }
+            }
+            if (Target != null)
+            {
+                if (tank.getDead())
+                {
+                    //  aqui va lo que quiera que haga el moco
+                    if (player is ReySlime)
+                    {
+                        ReySlime moco = (ReySlime)player;
+                        moco.disparoMoco(Target);
+
+                    }
+
+                    //  all[Target].cambiaHp(10000, 'f');                  
+
+                }
+                else
+                {
+                    if (Target is tankBase)
+                    {
+                        //  aqui va lo que quiera que haga el moco
+                        if (player is ReySlime)
+                        {
+                            ReySlime moco = (ReySlime)player;
+                            moco.disparoMoco(Target);
+
+                        }
+                    }
+                }
+
+                player.restaCooldowns();
+                StartCoroutine(waiter());
+
+            }
+
+            for (int i = 0; i < all.Length; i++)
+            {
+                all[i].clicked = false;
+            }
+        }
+    }
+
+    void AccionB()  //  Lluvia Acida (5 adversarii)
+    {
+        if (currentState == BattleStates.ENEMYC)
+        {
+            if (player is ReySlime)
+            {
+                ReySlime Moco = (ReySlime)player;
+                Moco.lluviaAcida(all);
+            }
+
+
+
+            player.restaCooldowns();
+            StartCoroutine(waiter());
+
+        }
+
+        for (int i = 0; i < all.Length; i++)
+        {
+            all[i].clicked = false;
+        }
+    }
+
+    void AccionC()
+    {
+
+        if (currentState == BattleStates.ENEMYC)
+        {
+
+            npc Target = null;
+
+            for (int i = 0; i < all.Length; i++)
+            {
+                if (all[i].clicked)
+                {
+                    Target = all[i];
+
+                }
+            }
+            if (Target != null)
+            {
+                if (tank.getDead())
+                {
+                    //  aqui va lo que quiera que haga el moco
+                    if (player is ReySlime)
+                    {
+                        ReySlime moco = (ReySlime)player;
+                        moco.disparoMoco(Target);
+
+                    }
+
+                    //  all[Target].cambiaHp(10000, 'f');                  
+
+                }
+                else
+                {
+                    if (Target is tankBase)
+                    {
+                        //  aqui va lo que quiera que haga el moco
+                        if (player is ReySlime)
+                        {
+                            ReySlime moco = (ReySlime)player;
+                            moco.regeneracion();
+
+                        }
+                    }
+                }
+
+                player.restaCooldowns();
+                StartCoroutine(waiter());
+
+            }
+
+            for (int i = 0; i < all.Length; i++)
+            {
+                all[i].clicked = false;
+            }
+
+        }
+    }
+
+    void AccionD()
+    {
+
+        if (currentState == BattleStates.ENEMYC)
+        {
+
+            npc Target = null;
+
+            for (int i = 0; i < all.Length; i++)
+            {
+                if (all[i].clicked)
+                {
+                    Target = all[i];
+
+                }
+            }
+            if (Target != null)
+            {
+                if (tank.getDead())
+                {
+                    //  aqui va lo que quiera que haga el moco
+                    if (player is ReySlime)
+                    {
+                        ReySlime moco = (ReySlime)player;
+                        moco.aCenar(Target);
+
+                    }
+
+                    //  all[Target].cambiaHp(10000, 'f');                  
+
+                }
+                else
+                {
+                    if (Target is tankBase)
+                    {
+                        //  aqui va lo que quiera que haga el moco
+                        if (player is ReySlime)
+                        {
+                            ReySlime moco = (ReySlime)player;
+                            moco.aCenar(Target);
+
+                        }
+                    }
+                }
+
+                player.restaCooldowns();
+                StartCoroutine(waiter());
+
+            }
+
+            for (int i = 0; i < all.Length; i++)
+            {
+                all[i].clicked = false;
+            }
+        }
+    }
+
+    void AccionE()  //  temblor wow
+    {
+
+        if (currentState == BattleStates.ENEMYC)
+        {
+            //  aqui va lo que quiera que haga el moco
+            if (player is ReySlime)
+            {
+                ReySlime moco = (ReySlime)player;
+                //moco.golpeTemblor(all[]);
+
+            }
+            player.restaCooldowns();
+            StartCoroutine(waiter());
+
+            for (int i = 0; i < all.Length; i++)
+            {
+                all[i].clicked = false;
+            }
+        }
+    }
+
+    void AccionF()
+    {
+
+        if (currentState == BattleStates.ENEMYC)
+        {
+            //  aqui va lo que quiera que haga el moco
+            if (player is ReySlime)
+            {
+                ReySlime moco = (ReySlime)player;
+                //moco.mocotralleta(all[]);
+
+            }
+            player.restaCooldowns();
+            StartCoroutine(waiter());
+
+            for (int i = 0; i < all.Length; i++)
+            {
+                all[i].clicked = false;
+            }
+        }
+    }
+
+
+    bool AllTargetsDestroyed()
+    {
+        if (tank.getDead() && dps.getDead() && healer.getDead() && espadachin.getDead())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 	void ataqueClick(){
 		if(currentState==BattleStates.ENEMYC){
 			foreach (npc h in all) {
@@ -135,6 +478,7 @@ public class main : MonoBehaviour {
 		}
 	}
 	IEnumerator waiter(){
+
         yield return new WaitForSeconds(1);
         if (currentState == BattleStates.ENEMYC)
         {
@@ -156,7 +500,7 @@ public class main : MonoBehaviour {
         {
             currentState = BattleStates.ENEMYC;
         }
-        
+
     }
     /*public void bucle()
 {
